e39444351f70215c99ecbc23bf74b10d703b827e
Maris Dautov <maris.dautov28@gmail.com>
2025-05-26 13:09:24 +0500

    add функция sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -12,3 +12,11 @@ def filter_by_state(transactions: list, state: str ='EXECUTED') ->list:
                 filtered_list.append(transaction)
 
     return filtered_list
+
+def sort_by_date(transactions: list, reverse_sort: bool = True) -> list:
+    """Сортирует список транзакций по дате"""
+    return sorted(
+        transactions,
+        key=lambda x: x["date"].split("T")[0],
+        reverse=reverse_sort
+    )
