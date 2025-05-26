81cb09b479c1ea76d79d1918790cc1e9bddd74fd
Maris Dautov <maris.dautov28@gmail.com>
2025-05-26 13:16:54 +0500

    проведены все проверки и исправления

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,19 @@
-def filter_by_state(transactions: list, state: str ='EXECUTED') ->list:
+def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция фильтрации словарей по ключу"""
     filtered_list = []
 
     # Перебираем все транзакции в цикле
     for transaction in transactions:
         # Проверяем наличие ключа 'state'
-        if 'state' in transaction:
+        if "state" in transaction:
             # Проверяем соответствие значения ключа 'state'
-            if transaction['state'] == state:
+            if transaction["state"] == state:
                 # Добавляем подходящую транзакцию в новый список
                 filtered_list.append(transaction)
 
     return filtered_list
 
-def sort_by_date(transactions: list, reverse_sort: bool = True) -> list:
+
+def sort_by_date(transactions: list[dict], reverse_sort: bool = True) -> list[dict]:
     """Сортирует список транзакций по дате"""
-    return sorted(
-        transactions,
-        key=lambda x: x["date"].split("T")[0],
-        reverse=reverse_sort
-    )
+    return sorted(transactions, key=lambda x: x["date"].split("T")[0], reverse=reverse_sort)
--- src/widget.py
+++ src/widget.py
@@ -28,5 +28,5 @@ def get_date(data_time: str) -> str:
     return f"{day}.{month}.{year}"
 
 
-print(mask_number(input("Введите данные")))
-print(get_date("2024-03-11T02:26:18.671407"))
+# print(mask_number(input("Введите данные")))
+# print(get_date("2024-03-11T02:26:18.671407"))
