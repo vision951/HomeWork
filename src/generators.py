ae6458aa455dc0ef1a2092e4b88501e01e10d02f
Maris Dautov <maris.dautov28@gmail.com>
2025-06-02 20:37:43 +0500

    fix func card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -17,11 +17,11 @@ def transaction_descriptions(transactions: list[dict]) -> Generator[str]:
         yield transaction.get("description", "Описание отсутствует")
 
 
-def card_number_generator(start: int, end: int) -> Generator[str]:
+def card_number_generator(start: int, stop: int) -> Generator[str]:
     """Генератор номеров банковских карт в заданном диапазоне"""
-    if not (1 <= start <= end <= 9999999999999999):
+    if not (1 <= start <= stop <= 9999999999999999):
         raise ValueError("Некорректный диапазон номеров карт")
 
-    for number in range(start, end + 1):
+    for number in range(start, stop + 1):
         card_str = f"{number:016d}"
         yield " ".join(card_str[i : i + 4] for i in range(0, 16, 4))
