ddb3308bc0c052d5144e00bff0a19cf11fc003b5
Maris Dautov <maris.dautov28@gmail.com>
2025-06-02 18:00:37 +0500

    fixes generators.py +  добавлены анотации, проведены проверки flake8, isort, mypy

--- src/generators.py
+++ src/generators.py
@@ -1,12 +1,14 @@
+from typing import Generator
+
+
 def filter_by_currency(transactions: list[dict], currency_code: str) -> filter:
     """Фильтрует транзакции по валюте и возвращает итератор."""
     return filter(
-        lambda t: t.get("operationAmount", {}).get("currency", {}).get("code") == currency_code,
-        transactions
+        lambda t: t.get("operationAmount", {}).get("currency", {}).get("code") == currency_code, transactions
     )
 
 
-def transaction_descriptions(transactions: list[dict]):
+def transaction_descriptions(transactions: list[dict]) -> Generator[str]:
     """Функция возвращает описание операции"""
     if not transactions:
         return
@@ -15,14 +17,11 @@ def transaction_descriptions(transactions: list[dict]):
         yield transaction.get("description", "Описание отсутствует")
 
 
-def card_number_generator(start: int, end: int) -> str:
+def card_number_generator(start: int, end: int) -> Generator[str]:
     """Генератор номеров банковских карт в заданном диапазоне"""
     if not (1 <= start <= end <= 9999999999999999):
         raise ValueError("Некорректный диапазон номеров карт")
 
     for number in range(start, end + 1):
         card_str = f"{number:016d}"
-        yield ' '.join(card_str[i:i + 4] for i in range(0, 16, 4))
-
-
-
+        yield " ".join(card_str[i : i + 4] for i in range(0, 16, 4))
