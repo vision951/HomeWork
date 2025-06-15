db3bce4a44ec52ec5d78adf4202a4340c4ea8752
Maris Dautov <maris.dautov28@gmail.com>
2025-06-15 15:24:59 +0500

    update decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -6,28 +6,25 @@ def log(filename=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            # Фиксируем начало
-            time_stamp = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
             # Логируем начало выполнения
             log_message(f"[{time_stamp}] {func.__name__}: Starting", filename)
 
             try:
                 # Выполняем функцию
                 result = func(*args, **kwargs)
-                # Фисируем окончание
-                time_stamp = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
                 # Логируем конец выполнения
                 log_message(f"[{time_stamp}] {func.__name__}: ok", filename)
                 return result
             except Exception as e:
-                # Фисируем окончание
-                time_stamp = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
                 error_type = type(e).__name__
                 # Логируем конец выполнения с ошибкой
                 log_message(f"[{time_stamp}] {func.__name__}: error: {error_type}. Inputs: {args}, {kwargs}", filename)
                 raise
         return wrapper
     return decorator
+
+
+time_stamp = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
 
 
 def log_message(message, filename=None):
